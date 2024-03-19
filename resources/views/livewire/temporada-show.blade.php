9785b598498e1311b09fe4d8c16f7bdc7b3d129b
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2024-03-19 18:19:06 -0300

    u

--- resources/views/livewire/temporada-show.blade.php
+++ resources/views/livewire/temporada-show.blade.php
@@ -486,7 +486,7 @@
                                               {{$razon->csg}}
                                               </td>
                                               <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                                <a href="{{route('exportpdff',['razonsocial'=>$razonsocial,'temporada'=>$temporada])}}" target="_blank">
+                                                <a href="{{route('exportpdff',['razonsocial'=>$razon,'temporada'=>$temporada])}}" target="_blank">
                                                   <x-button>
                                                     Generar
                                                   </x-button>
