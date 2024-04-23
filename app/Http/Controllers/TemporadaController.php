ad77201bae82fce40e710c4ad78648f623e995d2
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2024-04-23 15:22:28 -0400

    u

--- app/Http/Controllers/TemporadaController.php
+++ app/Http/Controllers/TemporadaController.php
@@ -357,9 +357,9 @@ class TemporadaController extends Controller
                         if(preg_replace('/[\.\-\s]+/', '', strtolower($fob->n_calibre))==preg_replace('/[\.\-\s]+/', '', strtolower($calibre))){
                             $nro1+=1; 
                         }
-                        
+                        $nro2+=1; 
                         if (preg_replace('/[\.\-\s]+/', '', strtolower($fob->n_calibre))==preg_replace('/[\.\-\s]+/', '', strtolower($calibre)) && preg_replace('/[\.\-\s]+/', '', strtolower($fob->etiqueta))==preg_replace('/[\.\-\s]+/', '', strtolower($masa->n_etiqueta)) && preg_replace('/[\.\-\s]+/', '', strtolower($fob->color))==preg_replace('/[\.\-\s]+/', '', strtolower($color))){
-                            $nro2+=1; 
+                          
                             //$masa->update(['precio_fob'=>$fob->fob_kilo_salida]);
                              
                         }
