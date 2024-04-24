113e52afe0a29e5c962f8339031891beddad11f7
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2024-04-24 13:36:49 -0400

    u

--- app/Http/Controllers/TemporadaController.php
+++ app/Http/Controllers/TemporadaController.php
@@ -359,9 +359,10 @@ class TemporadaController extends Controller
                 }
                 $nro2+=1; 
 
+                /*
                 if($masa->id==71991){
                     $etiq[]=$masa->n_calibre.'-'.$color.'-'.$masa->semana.'-E.MASA'.$masa->n_etiqueta;
-                }
+                }*/
 
                
                        
@@ -401,13 +402,13 @@ class TemporadaController extends Controller
                           
                         if ((preg_replace('/[\.\-\s]+/', '', strtolower($fob->n_calibre))==preg_replace('/[\.\-\s]+/', '', strtolower($calibre))) && (preg_replace('/[\.\-\s]+/', '', strtolower($fob->etiqueta))==preg_replace('/[\.\-\s]+/', '', strtolower($masa->n_etiqueta))) && (preg_replace('/[\.\-\s]+/', '', strtolower($fob->color))==preg_replace('/[\.\-\s]+/', '', strtolower($color))) && (preg_replace('/[\.\-\s]+/', '', strtolower($fob->categoria))==preg_replace('/[\.\-\s]+/', '', strtolower($masa->n_categoria)))){
                           
-                           // $masa->update(['precio_fob'=>$fob->fob_kilo_salida]);
+                        $masa->update(['precio_fob'=>$fob->fob_kilo_salida]);
                            $nro1+=1; 
                         }
                     }
                 }
         }
-        /*
+        
         foreach($masascati as $masa){
             if ($masa->n_calibre=='4J' || $masa->n_calibre=='4JD' || $masa->n_calibre=='4JDD'){
                 $calibre='4J';
@@ -468,8 +469,8 @@ class TemporadaController extends Controller
         
 
         }
-        */
-        dd($etiq);
+        
+       // dd($etiq);
         return redirect()->back()->with('info',$nro1.'-'.$cali.'-'.$etiqueta.'-'.$col.'-'.$categoria.'-'.$suma.'/'.$nro2.' Actualizados con Éxito');
     }
 
