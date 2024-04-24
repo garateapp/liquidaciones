86804596c39bbd4ddca96f4afb0d8ffc028d6779
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2024-04-24 16:43:29 -0400

    u

--- app/Models/Balancemasa.php
+++ app/Models/Balancemasa.php
@@ -42,21 +42,25 @@ class Balancemasa extends Model
     }
 
     public function scopeFilter2($query,$filters){
-        $query->where('n_categoria','!=','Cat 1')->where('n_categoria','!=','Cat I')->when($filters['razonsocial'] ?? null, function ($query, $serie) {
+        $query->where(function ($query) {
+            $query->where('n_categoria', '!=', 'Cat 1')
+                ->where('n_categoria', '!=', 'Cat I');
+        })->when($filters['razonsocial'] ?? null, function ($query, $serie) {
             $query->where('c_embalaje', 'like', '%' . $serie . '%')
                 ->orWhere('n_productor', 'like', '%' . $serie . '%')
                 ->orWhere('r_productor', 'like', '%' . $serie . '%');
         })->when($filters['precioFob'] ?? null, function ($query, $precioFob) {
-            if ($precioFob == 'null') {
+            if ($precioFob === 'null') {
                 $query->whereNull('precio_fob');
             }
         })->when($filters['ncategoria'] ?? null, function ($query, $nCategoria) {
             $query->where('n_categoria_st', $nCategoria);
-        })->when($filters['calibre'] ?? null,function($query,$calibre){
-            $query->where('n_calibre',$calibre);
-        })->when($filters['variedad'] ?? null,function($query,$variedad){
-            $query->where('n_variedad',$variedad);
+        })->when($filters['calibre'] ?? null, function ($query, $calibre) {
+            $query->where('n_calibre', $calibre);
+        })->when($filters['variedad'] ?? null, function ($query, $variedad) {
+            $query->where('n_variedad', $variedad);
         });
+        
     }
     
 }
