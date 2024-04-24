23b52d7400b26b032839cf5cbf7d4d2f9672539b
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2024-04-24 17:39:09 -0400

    u

--- resources/views/livewire/temporada-show.blade.php
+++ resources/views/livewire/temporada-show.blade.php
@@ -18,7 +18,7 @@
         <button class="inline-flex items-center px-4 mr-2 py-2 bg-red-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-700 focus:bg-red-700 active:bg-red-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
           EXPORTACIÓN
         </button>
-      @else
+      @elseif($vista=="resumesnacional")
         <a href="{{Route('temporadas.show',$temporada)}}" wire:navigate>
           <button class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
             EXPORTACIÓN
@@ -29,7 +29,7 @@
         <button class="inline-flex items-center px-4 ml-2 py-2 bg-red-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-700 focus:bg-red-700 active:bg-red-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
           NACIONAL
         </button>
-      @else
+      @elseif($vista=="resumes")
         <a href="{{Route('temporada.nacional',$temporada)}}" wire:navigate>
           <button class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
             NACIONAL
