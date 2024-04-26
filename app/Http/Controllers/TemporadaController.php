4df9c3ff29024338eff73230b4e46ffeffdafda6
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2024-04-25 21:22:07 -0400

    u

--- app/Http/Controllers/TemporadaController.php
+++ app/Http/Controllers/TemporadaController.php
@@ -315,7 +315,7 @@ class TemporadaController extends Controller
         $categoria=0;
         $etiq=[];
         $suma=0;
-
+        
         foreach ($fobsall as $fob){
             foreach($masascat1 as $masa){
                 if ($masa->n_calibre=='4J' || $masa->n_calibre=='4JD' || $masa->n_calibre=='4JDD'){
@@ -385,12 +385,12 @@ class TemporadaController extends Controller
                     }
                 }
                 $nro2+=1; 
-                $a=$masa->n_variedad;
+
                 /*
                 if($masa->id==71991){
                     $etiq[]=$masa->n_calibre.'-'.$color.'-'.$masa->semana.'-E.MASA'.$masa->n_etiqueta;
                 }*/
-                $b=$fob->n_variedad;
+                
                         if ((str_replace(' ', '', $fob->n_variedad)==str_replace(' ', '', $masa->n_variedad)) && $fob->semana==$masa->semana && (preg_replace('/[\.\-\s]+/', '', strtolower($fob->n_calibre))==preg_replace('/[\.\-\s]+/', '', strtolower($calibre))) && (preg_replace('/[\.\-\s]+/', '', strtolower($fob->etiqueta))==preg_replace('/[\.\-\s]+/', '', strtolower($masa->n_etiqueta))) && (preg_replace('/[\.\-\s]+/', '', strtolower($fob->color))==preg_replace('/[\.\-\s]+/', '', strtolower($color))) && (preg_replace('/[\.\-\s]+/', '', strtolower($fob->categoria))==preg_replace('/[\.\-\s]+/', '', strtolower($masa->n_categoria)))){
                           
                             if($fob->fob_kilo_salida!='null'){
@@ -399,8 +399,8 @@ class TemporadaController extends Controller
                             }
                         }else{
 
-                            $fob=Fob::where('temporada_id',$masa->temporada_id)->where('n_variedad',$masa->n_variedad)->where('semana',$masa->semana)->where('etiqueta',$masa->n_etiqueta)->where('n_calibre',$masa->n_calibre)->where('color',$color)->where('categoria',$masa->n_categoria)->first();
-                            if ($fob) {
+                            $test=Fob::where('temporada_id',$masa->temporada_id)->where('n_variedad',$masa->n_variedad)->where('semana',$masa->semana)->where('etiqueta',$masa->n_etiqueta)->where('n_calibre',$masa->n_calibre)->where('color',$color)->where('categoria',$masa->n_categoria)->first();
+                            if ($test) {
                                
                             } else {
                                 Fob::create([ 
@@ -503,8 +503,8 @@ class TemporadaController extends Controller
                                 $nro1+=1; 
                             }
                         }else{
-                            $fob=Fob::where('temporada_id',$masa->temporada_id)->where('n_variedad',$masa->n_variedad)->where('semana',$masa->semana)->where('etiqueta',$masa->n_etiqueta)->where('n_calibre',$masa->n_calibre)->where('color',$color)->where('categoria',$masa->n_categoria)->first();
-                            if ($fob) {
+                            $test=Fob::where('temporada_id',$masa->temporada_id)->where('n_variedad',$masa->n_variedad)->where('semana',$masa->semana)->where('etiqueta',$masa->n_etiqueta)->where('n_calibre',$masa->n_calibre)->where('color',$color)->where('categoria',$masa->n_categoria)->first();
+                            if ($test) {
                                
                             } else {
                                 Fob::create([ 
