195f66750b217a371e3e3fe79ebb2c932436dc56
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2024-04-25 20:46:31 -0400

    u

--- app/Http/Controllers/TemporadaController.php
+++ app/Http/Controllers/TemporadaController.php
@@ -303,8 +303,8 @@ class TemporadaController extends Controller
     }
 
     public function fobupdate(Temporada $temporada)
-    {   $masascat1=Balancemasa::where('temporada_id',$temporada->id)->where('n_categoria','Cat 1')->whereNull('precio_fob')->paginate(5000);
-        $masascati=Balancemasa::where('temporada_id',$temporada->id)->where('n_categoria','Cat I')->whereNull('precio_fob')->paginate(5000);
+    {   $masascat1=Balancemasa::where('temporada_id',$temporada->id)->where('n_categoria','Cat 1')->whereNull('precio_fob')->paginate(1000);
+        $masascati=Balancemasa::where('temporada_id',$temporada->id)->where('n_categoria','Cat I')->whereNull('precio_fob')->paginate(1000);
        
         $nro1=0;
         $nro2=0;
