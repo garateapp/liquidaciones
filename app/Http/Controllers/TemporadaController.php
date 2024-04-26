68bf702848ca1fd2b45f282c180afd88cb12b6da
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2024-04-25 21:14:29 -0400

    u

--- app/Http/Controllers/TemporadaController.php
+++ app/Http/Controllers/TemporadaController.php
@@ -315,6 +315,7 @@ class TemporadaController extends Controller
         $categoria=0;
         $etiq=[];
         $suma=0;
+        
         foreach ($fobsall as $fob){
             foreach($masascat1 as $masa){
                 if ($masa->n_calibre=='4J' || $masa->n_calibre=='4JD' || $masa->n_calibre=='4JDD'){
@@ -389,13 +390,6 @@ class TemporadaController extends Controller
                 if($masa->id==71991){
                     $etiq[]=$masa->n_calibre.'-'.$color.'-'.$masa->semana.'-E.MASA'.$masa->n_etiqueta;
                 }*/
-
-               
-                       
-                
-                    
-                   
-                          
                         if ((str_replace(' ', '', $fob->n_variedad)==str_replace(' ', '', $masa->n_variedad)) && $fob->semana==$masa->semana && (preg_replace('/[\.\-\s]+/', '', strtolower($fob->n_calibre))==preg_replace('/[\.\-\s]+/', '', strtolower($calibre))) && (preg_replace('/[\.\-\s]+/', '', strtolower($fob->etiqueta))==preg_replace('/[\.\-\s]+/', '', strtolower($masa->n_etiqueta))) && (preg_replace('/[\.\-\s]+/', '', strtolower($fob->color))==preg_replace('/[\.\-\s]+/', '', strtolower($color))) && (preg_replace('/[\.\-\s]+/', '', strtolower($fob->categoria))==preg_replace('/[\.\-\s]+/', '', strtolower($masa->n_categoria)))){
                           
                             if($fob->fob_kilo_salida!='null'){
