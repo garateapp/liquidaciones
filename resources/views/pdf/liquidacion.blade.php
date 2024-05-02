ab4ba4b023fb640820c1469f939e96b6b7c03eef
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2024-05-02 16:59:39 -0400

    u

--- resources/views/pdf/liquidacion.blade.php
+++ resources/views/pdf/liquidacion.blade.php
@@ -4,7 +4,7 @@
 	<title>Liq. {{$razonsocial->name}}</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 	<link href=”https://fonts.googleapis.com/css?family=Pacifico” rel=”stylesheet”>
-	<link rel="shortcut icon" href="https://greenexweb.cl/image/logogreenex.png">
+	<link rel="shortcut icon" href="{{asset('image/iconogreenex.png')}}">
 	<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 
 	<style>
