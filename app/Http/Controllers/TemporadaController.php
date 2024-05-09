e93a29937290aa7d4bf31ba08b450936dae67cb8
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2024-05-09 17:18:44 -0400

    u

--- app/Http/Controllers/TemporadaController.php
+++ app/Http/Controllers/TemporadaController.php
@@ -822,6 +822,14 @@ class TemporadaController extends Controller
                                 if($fob->fob_kilo_salida!='null'){
                                     $masa->update(['precio_fob'=>$fob->fob_kilo_salida]);
                                     $nro1+=1; 
+                                    break;
+                                }
+                            }elseif ((str_replace(' ', '', $fob->n_variedad)==str_replace(' ', '', $masa->n_variedad)) && (preg_replace('/[\.\-\s]+/', '', strtolower($fob->n_calibre))==preg_replace('/[\.\-\s]+/', '', strtolower($calibre))) && (preg_replace('/[\.\-\s]+/', '', strtolower($fob->etiqueta))==preg_replace('/[\.\-\s]+/', '', strtolower($masa->n_etiqueta))) && (preg_replace('/[\.\-\s]+/', '', strtolower($fob->color))==preg_replace('/[\.\-\s]+/', '', strtolower($color))) && (preg_replace('/[\.\-\s]+/', '', strtolower($fob->categoria))==preg_replace('/[\.\-\s]+/', '', strtolower($masa->n_categoria)))){
+                            
+                                if($fob->fob_kilo_salida!='null'){
+                                    $masa->update(['precio_fob'=>$fob->fob_kilo_salida]);
+                                    $nro1+=1; 
+                                    break;
                                 }
                             }
                           
@@ -904,6 +912,13 @@ class TemporadaController extends Controller
                                 if($fob->fob_kilo_salida!='null'){
                                     $masa->update(['precio_fob'=>$fob->fob_kilo_salida]);
                                     $nro1+=1; 
+                                    break;
+                                }
+                            }elseif ((str_replace(' ', '', $fob->n_variedad)==str_replace(' ', '', $masa->n_variedad)) && (preg_replace('/[\.\-\s]+/', '', strtolower($fob->n_calibre))==preg_replace('/[\.\-\s]+/', '', strtolower($calibre))) && (preg_replace('/[\.\-\s]+/', '', strtolower($fob->etiqueta))==preg_replace('/[\.\-\s]+/', '', strtolower($masa->n_etiqueta))) && (preg_replace('/[\.\-\s]+/', '', strtolower($fob->color))==preg_replace('/[\.\-\s]+/', '', strtolower($color))) && (preg_replace('/[\.\-\s]+/', '', strtolower($fob->categoria))==preg_replace('/[\.\-\s]+/', '', strtolower($masa->n_categoria)))){
+                                if($fob->fob_kilo_salida!='null'){
+                                    $masa->update(['precio_fob'=>$fob->fob_kilo_salida]);
+                                    $nro1+=1; 
+                                    break;
                                 }
                             }
                           
