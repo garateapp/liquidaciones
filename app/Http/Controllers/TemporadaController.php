f4f59df17484d38a03845206e2d5480e8d654fc3
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2024-05-09 15:51:40 -0400

    u

--- app/Http/Controllers/TemporadaController.php
+++ app/Http/Controllers/TemporadaController.php
@@ -963,9 +963,10 @@ class TemporadaController extends Controller
 
     public function fobcreate(Temporada $temporada)
     {   $masascat1=Balancemasa::where('temporada_id',$temporada->id)->where('n_categoria','Cat 1')->whereNull('precio_fob')->paginate(5000);
-      
-        $fobscat1=Fob::where('temporada_id',$temporada->id)->where('categoria','Cat 1')->get();
-        $fobscati=Fob::where('temporada_id',$temporada->id)->where('categoria','Cat I')->get();
+        
+        $fobscat1=Fob::where('temporada_id',$temporada->id)->where('categoria','CAT1')->get();
+        $fobscati=Fob::where('temporada_id',$temporada->id)->where('categoria','CAT I')->get();
+
         $nro1=0;
         $nro2=0;
         $nro3=0;
@@ -1082,7 +1083,6 @@ class TemporadaController extends Controller
                     }
             }
 
-            
         $masascati=Balancemasa::where('temporada_id',$temporada->id)->where('n_categoria','Cat I')->whereNull('precio_fob')->paginate(5000);
             foreach($masascati as $masa){
                 $color=null;
