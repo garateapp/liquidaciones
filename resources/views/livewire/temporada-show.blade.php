dcfe42004aa26d6bee16c3c9fe84538878e8def5
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2024-06-20 13:40:40 -0400

    u

--- resources/views/livewire/temporada-show.blade.php
+++ resources/views/livewire/temporada-show.blade.php
@@ -660,7 +660,7 @@
 
              
                 <div wire:loading wire:target="filters">
-                  <div class="fixed left-0 top-0 flex h-full w-full items-center justify-center bg-gray-300 bg-opacity-10  py-10" >
+                  <div class="fixed left-0 top-0 flex h-full w-full items-center justify-center bg-gray-800 bg-opacity-50 py-10" >
                     
                     <div class="max-h-full w-full max-w-xl overflow-y-auto mx-auto sm:rounded-2xl bg-white">
                       <div class="w-full">
