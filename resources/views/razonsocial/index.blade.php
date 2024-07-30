f086f7c18d97dbc89a5da9e4d9be5d24d40902bc
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2024-07-30 08:18:03 -0400

    u

--- resources/views/razonsocial/index.blade.php
+++ resources/views/razonsocial/index.blade.php
@@ -20,6 +20,31 @@
                     <div class="overflow-x-auto sm:mx-0.5 lg:mx-0.5">
                       <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
                         <div class="overflow-hidden">
+                          @php
+                             $etiquetas = [
+                                  'comision_exportadora' => 'Comisión (Exportadora)',
+                                  'beneficio_flete_huerto' => [
+                                      'si' => 'Existe Beneficio Flete a Huerto (Sí)',
+                                      'no' => 'Existe Beneficio Flete a Huerto (No)',
+                                      '50' => 'Existe Beneficio Flete a Huerto (50%)'
+                                  ],
+                                  'minimo_garantizado' => 'Mínimo Garantizado',
+                                  'rebate' => [
+                                      'si_aplica' => 'Rebate (Sí Aplica)',
+                                      'no_aplica' => 'Rebate (No Aplica)'
+                                  ],
+                                  'descuento_fruta_comercial' => [
+                                      'si' => 'Descuento Fruta Comercial (Sí)',
+                                      'no' => 'Descuento Fruta Comercial (No)',
+                                      'porcentaje' => 'Descuento Fruta Comercial (%)'
+                                  ],
+                                  'liquidacion' => [
+                                      'si' => 'Liquidación (Sí)',
+                                      'no' => 'Liquidación (No)'
+                                  ]
+                              ];
+
+                          @endphp
                           <table class="min-w-full">
                             <thead class="bg-white border-b">
                               <tr>
@@ -28,6 +53,9 @@
                                 </th>
                                 <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
                                   Name
+                                </th>
+                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                  Condición
                                 </th>
                                 <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
                                   Rut
@@ -43,6 +71,9 @@
                                         <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{{$razon->id}}</td>
                                         <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                                         {{$razon->name}}
+                                        </td>
+                                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                          {!! Form::select('categoria', $etiquetas, null, ['class'=>'mt-1 block w-full rounded-lg', 'placeholder'=>'¿Condición?']) !!}
                                         </td>
                                         <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                                         {{$razon->rut}}
