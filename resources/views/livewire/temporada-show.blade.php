b74968a79c49af6e04c8abf1eb73eb902ebf002e
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2024-08-05 16:11:13 -0400

    u

--- resources/views/livewire/temporada-show.blade.php
+++ resources/views/livewire/temporada-show.blade.php
@@ -2615,8 +2615,7 @@
                                         <path class="heroicon-ui"
                                               d="M12 12a5 5 0 1 1 0-10 5 5 0 0 1 0 10zm0-2a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm9 11a1 1 0 0 1-2 0v-2a3 3 0 0 0-3-3H8a3 3 0 0 0-3 3v2a1 1 0 0 1-2 0v-2a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v2z"/>
                                     </svg>
-                                    <input type="text" placeholder="https://api.greenexweb.cl/api/shipments"
-                                          class="bg-gray-300 max-w-full focus:outline-none text-gray-700"/>
+                                    <input type="text" value="https://api.greenexweb.cl/api/shipments"  class="bg-gray-300 flex w-full focus:outline-none text-gray-700"/>
                                 </div>
                           
                             </div>
@@ -2658,8 +2657,7 @@
                                             <path class="heroicon-ui"
                                                   d="M12 12a5 5 0 1 1 0-10 5 5 0 0 1 0 10zm0-2a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm9 11a1 1 0 0 1-2 0v-2a3 3 0 0 0-3-3H8a3 3 0 0 0-3 3v2a1 1 0 0 1-2 0v-2a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v2z"/>
                                         </svg>
-                                        <input type="text" placeholder="https://api.greenexweb.cl/api/dispatches"
-                                              class="bg-gray-300 max-w-full focus:outline-none text-gray-700"/>
+                                        <input type="text" value="https://api.greenexweb.cl/api/dispatches" class="bg-gray-300 flex w-full focus:outline-none text-gray-700"/>
                                     </div>
                               
                                 </div>
