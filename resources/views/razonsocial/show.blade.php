db90148fcbf76f270f7f3fdebc701c8143df335e
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2024-08-22 17:20:17 -0400

    u

--- resources/views/razonsocial/show.blade.php
+++ resources/views/razonsocial/show.blade.php
@@ -4,19 +4,19 @@
     <div class="pb-8 pt-2">
     
       <div class="">
-        <div class="px-2 md:px-2 py-4">
+        <div class="px-2 md:px-2 py-4 w-full">
         
         
-          <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+          <div class="flex items-center justify-center min-h-screen bg-gray-100 w-full">
        
             <!-- dark theme -->
-            <div class="container  m-4">
+            <div class="  m-4">
               <div class="max-w-7xl w-full mx-auto grid gap-4 grid-cols-1">
                 <a href="{{Route('razonsocial.index')}}">
                   <h1>Volver al listado de Productores</h1>
                 </a>
                 <!-- profile card -->
-                <div class="flex flex-col sticky top-0 z-10">
+                <div class="flex sticky top-0 z-10">
                   <div class="bg-gray-800 border border-gray-800 shadow-lg  rounded-2xl p-4">
                     <div class="flex-none sm:flex">
                       <div class=" relative h-32 w-32   sm:mb-0 mb-3">
@@ -112,112 +112,13 @@
                 </div>
 
                 <div class="grid gap-4 grid-cols-1 md:grid-cols-2">
-                  <h1 class="text-xl">Condiciones</h1>
                   <h1 class="text-xl">Csg</h1>
 
                 </div>
               
-                <div class="grid gap-4 grid-cols-1 md:grid-cols-2">
-                  <!--elements-->
-                  <div class="flex flex-col space-y-4">
-                    <!-- elements 1 -->
-                    <div class="flex flex-col p-4 bg-gray-800 border-gray-800 shadow-md hover:shodow-lg rounded-2xl cursor-pointer transition ease-in duration-500  transform hover:scale-105">
-          
-                      <div class="flex items-center justify-between">
-                        <div class="flex items-center mr-auto">
-          
-                          <div class="inline-flex w-12 h-12"><img src="https://tailwindcomponents.com/storage/avatars/njkIbPhyZCftc4g9XbMWwVsa7aGVPajYLRXhEeoo.jpg" alt="aji" class=" relative w-12 h-12 object-cover rounded-2xl"><span class="animate-ping absolute w-12 h-12 inline-flex border-2 rounded-2xl border-green-400 opacity-75"></span>
-                            <span></span>
-                          </div>
-          
-                          <div class="flex flex-col ml-3">
-                            <div class="font-medium leading-none text-gray-100">Aji</div>
-                            <p class="text-sm text-gray-500 leading-none mt-1">UI/UX Designer</p>
-                          </div>
-                        </div>
-                        <a href="#" class="flex-no-shrink text-xs  font-medium tracking-wider  text-gray-400 hover:text-green-400 transition ease-in duration-300 mr-2"><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                            <path fill-rule="evenodd" d="M4 5a2 2 0 00-2 2v8a2 2 0 002 2h12a2 2 0 002-2V7a2 2 0 00-2-2h-1.586a1 1 0 01-.707-.293l-1.121-1.121A2 2 0 0011.172 3H8.828a2 2 0 00-1.414.586L6.293 4.707A1 1 0 015.586 5H4zm6 9a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd"></path>
-                          </svg></a>
-                        <a hrf="#" class="flex-no-shrink text-xs  font-medium tracking-wider  text-gray-400 hover:text-green-400 transition ease-in duration-300 ml-2"><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                            <path fill-rule="evenodd" d="M7 4a3 3 0 016 0v4a3 3 0 11-6 0V4zm4 10.93A7.001 7.001 0 0017 8a1 1 0 10-2 0A5 5 0 015 8a1 1 0 00-2 0 7.001 7.001 0 006 6.93V17H6a1 1 0 100 2h8a1 1 0 100-2h-3v-2.07z" clip-rule="evenodd"></path>
-                          </svg></a>
-                      </div>
-                    </div>
-                    <!--elements 2-->
-                    <div class="flex flex-col p-4 bg-gray-800 border-gray-800 shadow-md hover:shodow-lg rounded-2xl cursor-pointer transition ease-in duration-500  transform hover:scale-105">
-          
-                      <div class="flex items-center justify-between">
-                        <div class="flex items-center mr-auto">
-          
-                          <div class="inline-flex w-12 h-12"><img src="https://tailwindcomponents.com/storage/avatars/njkIbPhyZCftc4g9XbMWwVsa7aGVPajYLRXhEeoo.jpg" alt="aji" class=" relative p-1 w-12 h-12 object-cover rounded-2xl"><span class="absolute w-12 h-12 inline-flex border-2 rounded-2xl border-green-400 opacity-75"></span>
-                            <span></span>
-                          </div>
-          
-                          <div class="flex flex-col ml-3 min-w-0">
-                            <div class="font-medium leading-none text-gray-100">Groupname</div>
-                            <p class="text-sm text-gray-500 leading-none mt-1 truncate">Beautiful hand-crafted SVG icons</p>
-                          </div>
-                        </div>
-                        <div class="flex flex-col ml-3 min-w-0">
-                          <span class="text-xs text-gray-500 text-right mb-1">9:02pm</span>
-                          <div class="flex">
-                            <a class="flex-no-shrink text-xs  font-medium tracking-wider  text-gray-400 hover:text-green-400 transition ease-in duration-300 mr-2"><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                                <path fill-rule="evenodd" d="M9.383 3.076A1 1 0 0110 4v12a1 1 0 01-1.707.707L4.586 13H2a1 1 0 01-1-1V8a1 1 0 011-1h2.586l3.707-3.707a1 1 0 011.09-.217zM12.293 7.293a1 1 0 011.414 0L15 8.586l1.293-1.293a1 1 0 111.414 1.414L16.414 10l1.293 1.293a1 1 0 01-1.414 1.414L15 11.414l-1.293 1.293a1 1 0 01-1.414-1.414L13.586 10l-1.293-1.293a1 1 0 010-1.414z" clip-rule="evenodd" />
-                              </svg></a>
-                            <span class="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-green-100 bg-green-400 rounded-full ml-2">99</span>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <!--elements 2-->
-                    <div class="flex flex-col p-4 bg-gray-800 border-gray-800 shadow-md hover:shodow-lg rounded-2xl cursor-pointer transition ease-in duration-500  transform hover:scale-105">
-                      <div class="flex items-center justify-between">
-                        <div class="flex items-center mr-auto">
-                          <div class="inline-flex w-12 h-12"><img src="https://tailwindcomponents.com/storage/avatars/njkIbPhyZCftc4g9XbMWwVsa7aGVPajYLRXhEeoo.jpg" alt="aji" class=" relative p-1 w-12 h-12 object-cover rounded-2xl"><span class="absolute w-12 h-12 inline-flex border-2 rounded-2xl border-gray-600 opacity-75"></span>
-                            <span></span>
-                          </div>
-          
-                          <div class="flex flex-col ml-3 min-w-0">
-                            <div class="font-medium leading-none text-gray-100">Ajimon</div>
-                            <p class="text-sm text-gray-500 leading-none mt-1 truncate">Jul 066, 2021, 8.25 PM</p>
-                          </div>
-                        </div>
-                        <div class="flex flex-col ml-3 min-w-0">
-                          <div class="flex">
-                            <h5 class="flex items-center font-medium text-gray-300 mr-2">
-                              <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 8h6m-5 0a3 3 0 110 6H9l3 3m-3-6h6m6 1a9 9 0 11-18 0 9 9 0 0118 0z" />
-                              </svg> 1800
-                            </h5>
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-green-400 ml-2" viewBox="0 0 20 20" fill="currentColor">
-                              <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
-                            </svg>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="flex flex-col p-4 bg-gray-800 border border-gray-800 shadow-md hover:text-green-500 text-gray-400 hover:shodow-lg rounded-2xl transition ease-in duration-500  transform hover:scale-105 cursor-pointer">
-                      <div class="flex items-center justify-between">
-                        <div class="flex items-center mr-auto">
-                          <div class="-space-x-5 flex ">
-                            <img src="https://tailwindcomponents.com/storage/avatars/njkIbPhyZCftc4g9XbMWwVsa7aGVPajYLRXhEeoo.jpg" alt="aji" class=" relative p-1 w-12 h-12 object-cover rounded-2xl border-2 border-gray-600 bg-gray-800">
-                                                <img src="https://tailwindcomponents.com/storage/avatars/njkIbPhyZCftc4g9XbMWwVsa7aGVPajYLRXhEeoo.jpg" alt="aji" class=" relative p-1 w-12 h-12 object-cover rounded-2xl border-2 border-gray-600 bg-gray-800 shadow">
-                          </div>
-          
-                          <div class="flex flex-col ml-3 min-w-0">
-                            <div class="font-medium leading-none text-gray-100">Pending Request </div>
-                            <p class="text-sm text-gray-500 leading-none mt-1 truncate">Jul 066, 2021, 8.25 PM</p>
-                          </div>
-                        </div>
-                        <div class="flex flex-col ml-3 min-w-0">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
-                    </svg>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="grid grid-cols-12 gap-4 ">
+                <div class="grid gap-4 grid-cols-1 md:grid-cols-2 w-full">
+               
+                  <div class="grid grid-cols-12 gap-4  col-span-2">
                     <div class="col-span-12 sm:col-span-4">
                       <div class="p-4 relative  bg-gray-800 border border-gray-800 shadow-lg  rounded-2xl">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-14 w-14  absolute bottom-4 right-3 text-green-400" viewBox="0 0 20 20" fill="currentColor">
@@ -256,6 +157,7 @@
                       </div>
                     </div>
                   </div>
+                  
 
                 </div>
 
@@ -263,7 +165,7 @@
 
                   <!---stats-->
                 
-                  <div class="flex flex-col">
+                  <div class="flex flex-col w-full">
                     <div class="lg:w-3/6 p-4 bg-gray-800 border-gray-800 shadow-md hover:shodow-lg rounded-2xl">
                       <div class="flex-none lg:flex">
                         <div class=" h-full w-full lg:h-24 lg:w-24   lg:mb-0 mb-3">
@@ -341,7 +243,7 @@
                       </div>
                     </div>
                   </div>
-                  
+
                
                   <div class="flex flex-col justify-center p-4 bg-gray-800 border-gray-800 shadow-md hover:shodow-lg rounded-2xl">
                     <div class="flex items-center justify-between">
