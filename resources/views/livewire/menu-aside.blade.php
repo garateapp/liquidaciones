4019882f72bb3248ece3587556ba6166433d838f
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2024-08-22 17:01:55 -0400

    u

--- resources/views/livewire/menu-aside.blade.php
+++ resources/views/livewire/menu-aside.blade.php
@@ -5,6 +5,79 @@
           
           style="display: none;">
         <div class="mb-6">
+          
+          <h1 class="mt-4">Bases de datos</h1>
+
+          <div class="grid grid-cols-1 gap-4 grid-cols-2 mt-2">
+            <!-- Start Navitem -->
+           
+        
+          
+              
+              @if (Route::currentRouteName() == 'temporada.balancemasa' || Route::currentRouteName() == 'temporada.recepcion' || Route::currentRouteName() == 'temporada.procesos')
+              @endif 
+              <a href="{{route('temporada.despachos',$temporada)}}"
+              wire:navigate class="col-span-2">
+                <div class="p-2 flex flex-col items-center bg-white rounded-md justify-center shadow-xl cursor-pointer">
+                  <div class="rounded-full p-2 @if($masascount->count()>0)bg-green-200 @else bg-indigo-200 @endif flex flex-col items-center">
+                    <i class="fas fa-shopping-basket fa-sm @if($masascount->count()>0)text-green-600 @else text-indigo-600 @endif"></i>
+                  </div>
+                  <p class="text-xs mt-1 text-center font-semibold">Despachos</p>
+                </div>
+              </a>
+                <a href="{{route('temporada.procesos',$temporada)}}"
+                wire:navigate class="col-span-2">
+                  <div class="p-2 flex flex-col items-center bg-white rounded-md justify-center shadow-xl cursor-pointer">
+                    <div class="rounded-full p-2 @if($masascount->count()>0)bg-green-200 @else bg-indigo-200 @endif flex flex-col items-center">
+                      <i class="fas fa-shopping-basket fa-sm @if($masascount->count()>0)text-green-600 @else text-indigo-600 @endif"></i>
+                    </div>
+                    <p class="text-xs mt-1 text-center font-semibold">Procesos</p>
+                  </div>
+                </a>
+                <a href="{{route('temporada.embarques',$temporada)}}"
+                wire:navigate class="col-span-2">
+                  <div class="p-2 flex flex-col items-center bg-white rounded-md justify-center shadow-xl cursor-pointer">
+                    <div class="rounded-full p-2 @if($masascount->count()>0)bg-green-200 @else bg-indigo-200 @endif flex flex-col items-center">
+                      <i class="fas fa-shopping-basket fa-sm @if($masascount->count()>0)text-green-600 @else text-indigo-600 @endif"></i>
+                    </div>
+                    <p class="text-xs mt-1 text-center font-semibold">Embarques</p>
+                  </div>
+                </a>
+                <a href="{{route('temporada.recepcion',$temporada)}}"
+                wire:navigate class="col-span-2">
+                  <div class="p-2 flex flex-col items-center bg-white rounded-md justify-center shadow-xl cursor-pointer">
+                    <div class="rounded-full p-2 @if($masascount->count()>0)bg-green-200 @else bg-indigo-200 @endif flex flex-col items-center">
+                      <i class="fas fa-shopping-basket fa-sm @if($masascount->count()>0)text-green-600 @else text-indigo-600 @endif"></i>
+                    </div>
+                    <p class="text-xs mt-1 text-center font-semibold">Recepciones</p>
+                  </div>
+                </a>
+             
+
+                <!-- Start Navitem -->
+            <a href="{{route('temporada.balancemasa',$temporada)}}"
+            wire:navigate>
+              <div class="p-2 flex flex-col items-center bg-white rounded-md justify-center shadow-xl cursor-pointer">
+                <div class="rounded-full p-2 @if($masascount->count()>0)bg-green-200 @else bg-indigo-200 @endif flex flex-col items-center">
+                  <i class="fas fa-shopping-basket fa-sm @if($masascount->count()>0)text-green-600 @else text-indigo-600 @endif"></i>
+                </div>
+                <p class="text-xs mt-1 text-center font-semibold">Balance de masa</p>
+              </div>
+            </a>
+
+              <!-- Start Navitem -->
+              <a href="{{route('temporada.fob',$temporada)}}"
+              wire:navigate>
+                <div class="p-2 flex flex-col items-center bg-white rounded-md justify-center shadow-xl cursor-pointer">
+                  <div class="rounded-full p-2 @if($temporada->fobs->count()>0)bg-green-200 @else bg-indigo-200 @endif flex flex-col items-center">
+                    <i class="fas fa-shopping-basket fa-sm @if($temporada->fobs->count()>0)text-green-600 @else text-indigo-600 @endif"></i>
+                  </div>
+                  <p class="text-xs mt-1 text-center font-semibold">Fob</p>
+                </div>
+              </a>
+        
+            <!-- End Navitem -->
+          </div>
           <!--Start Sidebar for open menu -->
           <h1 class="mt-4">Resumen</h1>
           <div class="grid grid-cols-1 gap-4 grid-cols-2 mt-2">
@@ -155,78 +228,6 @@
           </div>
           
 
-          <h1 class="mt-4">Balance de masa</h1>
-
-          <div class="grid grid-cols-1 gap-4 grid-cols-2 mt-2">
-            <!-- Start Navitem -->
-           
-        
-          
-              
-              @if (Route::currentRouteName() == 'temporada.balancemasa' || Route::currentRouteName() == 'temporada.recepcion' || Route::currentRouteName() == 'temporada.procesos')
-              @endif 
-              <a href="{{route('temporada.despachos',$temporada)}}"
-              wire:navigate class="col-span-2">
-                <div class="p-2 flex flex-col items-center bg-white rounded-md justify-center shadow-xl cursor-pointer">
-                  <div class="rounded-full p-2 @if($masascount->count()>0)bg-green-200 @else bg-indigo-200 @endif flex flex-col items-center">
-                    <i class="fas fa-shopping-basket fa-sm @if($masascount->count()>0)text-green-600 @else text-indigo-600 @endif"></i>
-                  </div>
-                  <p class="text-xs mt-1 text-center font-semibold">Despachos</p>
-                </div>
-              </a>
-                <a href="{{route('temporada.procesos',$temporada)}}"
-                wire:navigate class="col-span-2">
-                  <div class="p-2 flex flex-col items-center bg-white rounded-md justify-center shadow-xl cursor-pointer">
-                    <div class="rounded-full p-2 @if($masascount->count()>0)bg-green-200 @else bg-indigo-200 @endif flex flex-col items-center">
-                      <i class="fas fa-shopping-basket fa-sm @if($masascount->count()>0)text-green-600 @else text-indigo-600 @endif"></i>
-                    </div>
-                    <p class="text-xs mt-1 text-center font-semibold">Procesos</p>
-                  </div>
-                </a>
-                <a href="{{route('temporada.embarques',$temporada)}}"
-                wire:navigate class="col-span-2">
-                  <div class="p-2 flex flex-col items-center bg-white rounded-md justify-center shadow-xl cursor-pointer">
-                    <div class="rounded-full p-2 @if($masascount->count()>0)bg-green-200 @else bg-indigo-200 @endif flex flex-col items-center">
-                      <i class="fas fa-shopping-basket fa-sm @if($masascount->count()>0)text-green-600 @else text-indigo-600 @endif"></i>
-                    </div>
-                    <p class="text-xs mt-1 text-center font-semibold">Embarques</p>
-                  </div>
-                </a>
-                <a href="{{route('temporada.recepcion',$temporada)}}"
-                wire:navigate class="col-span-2">
-                  <div class="p-2 flex flex-col items-center bg-white rounded-md justify-center shadow-xl cursor-pointer">
-                    <div class="rounded-full p-2 @if($masascount->count()>0)bg-green-200 @else bg-indigo-200 @endif flex flex-col items-center">
-                      <i class="fas fa-shopping-basket fa-sm @if($masascount->count()>0)text-green-600 @else text-indigo-600 @endif"></i>
-                    </div>
-                    <p class="text-xs mt-1 text-center font-semibold">Recepciones</p>
-                  </div>
-                </a>
-             
-
-                <!-- Start Navitem -->
-            <a href="{{route('temporada.balancemasa',$temporada)}}"
-            wire:navigate>
-              <div class="p-2 flex flex-col items-center bg-white rounded-md justify-center shadow-xl cursor-pointer">
-                <div class="rounded-full p-2 @if($masascount->count()>0)bg-green-200 @else bg-indigo-200 @endif flex flex-col items-center">
-                  <i class="fas fa-shopping-basket fa-sm @if($masascount->count()>0)text-green-600 @else text-indigo-600 @endif"></i>
-                </div>
-                <p class="text-xs mt-1 text-center font-semibold">Balance de masa</p>
-              </div>
-            </a>
-
-              <!-- Start Navitem -->
-              <a href="{{route('temporada.fob',$temporada)}}"
-              wire:navigate>
-                <div class="p-2 flex flex-col items-center bg-white rounded-md justify-center shadow-xl cursor-pointer">
-                  <div class="rounded-full p-2 @if($temporada->fobs->count()>0)bg-green-200 @else bg-indigo-200 @endif flex flex-col items-center">
-                    <i class="fas fa-shopping-basket fa-sm @if($temporada->fobs->count()>0)text-green-600 @else text-indigo-600 @endif"></i>
-                  </div>
-                  <p class="text-xs mt-1 text-center font-semibold">Fob</p>
-                </div>
-              </a>
-        
-            <!-- End Navitem -->
-          </div>
 
          
           <!--End Sidebar for open menu -->
