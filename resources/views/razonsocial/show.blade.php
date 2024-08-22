d8290ecf872890c00d9ed632c925cd592ac03731
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2024-08-22 17:21:55 -0400

    Update

--- resources/views/razonsocial/show.blade.php
+++ resources/views/razonsocial/show.blade.php
@@ -7,16 +7,16 @@
         <div class="px-2 md:px-2 py-4 w-full">
         
         
-          <div class="flex items-center justify-center min-h-screen bg-gray-100 w-full">
+          <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100 w-full">
        
             <!-- dark theme -->
-            <div class="  m-4">
+            <div class="w-full m-4">
               <div class="max-w-7xl w-full mx-auto grid gap-4 grid-cols-1">
                 <a href="{{Route('razonsocial.index')}}">
                   <h1>Volver al listado de Productores</h1>
                 </a>
                 <!-- profile card -->
-                <div class="flex sticky top-0 z-10">
+                <div class="flex flex-col sticky top-0 z-10 w-full">
                   <div class="bg-gray-800 border border-gray-800 shadow-lg  rounded-2xl p-4">
                     <div class="flex-none sm:flex">
                       <div class=" relative h-32 w-32   sm:mb-0 mb-3">
@@ -119,7 +119,7 @@
                 <div class="grid gap-4 grid-cols-1 md:grid-cols-2 w-full">
                
                   <div class="grid grid-cols-12 gap-4  col-span-2">
-                    <div class="col-span-12 sm:col-span-4">
+                    <div class="col-span-12 sm:col-span-2">
                       <div class="p-4 relative  bg-gray-800 border border-gray-800 shadow-lg  rounded-2xl">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-14 w-14  absolute bottom-4 right-3 text-green-400" viewBox="0 0 20 20" fill="currentColor">
                           <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z" />
@@ -132,7 +132,7 @@
                         <div class="text-sm text-gray-500">Components</div>
                       </div>
                     </div>
-                    <div class="col-span-12 sm:col-span-4">
+                    <div class="col-span-12 sm:col-span-2">
                       <div class="p-4 relative  bg-gray-800 border border-gray-800 shadow-lg  rounded-2xl">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-14 w-14  absolute bottom-4 right-3 text-blue-500" viewBox="0 0 20 20" fill="currentColor">
                           <path d="M2 11a1 1 0 011-1h2a1 1 0 011 1v5a1 1 0 01-1 1H3a1 1 0 01-1-1v-5zM8 7a1 1 0 011-1h2a1 1 0 011 1v9a1 1 0 01-1 1H9a1 1 0 01-1-1V7zM14 4a1 1 0 011-1h2a1 1 0 011 1v12a1 1 0 01-1 1h-2a1 1 0 01-1-1V4z" />
@@ -144,7 +144,7 @@
                         <div class="text-sm text-gray-500">Projects</div>
                       </div>
                     </div>
-                    <div class="col-span-12 sm:col-span-4">
+                    <div class="col-span-12 sm:col-span-2">
                       <div class="p-4 relative  bg-gray-800 border border-gray-800 shadow-lg  rounded-2xl">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-14 w-14  absolute bottom-4 right-3 text-yellow-300" viewBox="0 0 20 20" fill="currentColor">
                           <path fill-rule="evenodd" d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
@@ -156,6 +156,44 @@
                         <div class="text-sm text-gray-500">Pen Items</div>
                       </div>
                     </div>
+                    <div class="col-span-12 sm:col-span-2">
+                      <div class="p-4 relative  bg-gray-800 border border-gray-800 shadow-lg  rounded-2xl">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-14 w-14  absolute bottom-4 right-3 text-green-400" viewBox="0 0 20 20" fill="currentColor">
+                          <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z" />
+                          <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z" />
+                        </svg>
+                        <div class="flex justify-between items-center ">
+                          <img class="w-7 filter grayscale" src="https://v1.tailwindcss.com/_next/static/media/tailwindcss-mark.6ea76c3b72656960a6ae5ad8b85928d0.svg" alt="taiwind css">
+                        </div>
+                        <div class="text-2xl text-gray-100 font-medium leading-8 mt-5">20</div>
+                        <div class="text-sm text-gray-500">Components</div>
+                      </div>
+                    </div>
+                    <div class="col-span-12 sm:col-span-2">
+                      <div class="p-4 relative  bg-gray-800 border border-gray-800 shadow-lg  rounded-2xl">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-14 w-14  absolute bottom-4 right-3 text-blue-500" viewBox="0 0 20 20" fill="currentColor">
+                          <path d="M2 11a1 1 0 011-1h2a1 1 0 011 1v5a1 1 0 01-1 1H3a1 1 0 01-1-1v-5zM8 7a1 1 0 011-1h2a1 1 0 011 1v9a1 1 0 01-1 1H9a1 1 0 01-1-1V7zM14 4a1 1 0 011-1h2a1 1 0 011 1v12a1 1 0 01-1 1h-2a1 1 0 01-1-1V4z" />
+                        </svg>
+                        <div class="flex justify-between items-center ">
+                          <i class="fab fa-behance text-xl text-gray-400"></i>
+                        </div>
+                        <div class="text-2xl text-gray-100 font-medium leading-8 mt-5">99</div>
+                        <div class="text-sm text-gray-500">Projects</div>
+                      </div>
+                    </div>
+                    <div class="col-span-12 sm:col-span-2">
+                      <div class="p-4 relative  bg-gray-800 border border-gray-800 shadow-lg  rounded-2xl">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-14 w-14  absolute bottom-4 right-3 text-yellow-300" viewBox="0 0 20 20" fill="currentColor">
+                          <path fill-rule="evenodd" d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                        </svg>
+                        <div class="flex justify-between items-center ">
+                          <i class="fab fa-codepen text-xl text-gray-400"></i>
+                        </div>
+                        <div class="text-2xl text-gray-100 font-medium leading-8 mt-5">50</div>
+                        <div class="text-sm text-gray-500">Pen Items</div>
+                      </div>
+                    </div>
+                    
                   </div>
                   
 
