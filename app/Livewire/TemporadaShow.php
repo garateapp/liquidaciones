e95ac56eb191583e8877d51628ecf2e49ba40585
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2024-08-30 18:46:31 -0400

    Update

--- app/Livewire/TemporadaShow.php
+++ app/Livewire/TemporadaShow.php
@@ -125,7 +125,9 @@ class TemporadaShow extends Component
             ->paginate($this->ctd);
 
             
-        $masastotal=Balancemasa::filter1($this->filters)->where('temporada_id',$this->temporada->id)->where('exportadora','Greenex SpA')->get();
+//        $masastotal=Recepcion::filter1($this->filters)->where('temporada_id',$this->temporada->id)->where('exportadora','Greenex SpA')->get();
+
+        $masastotal=Recepcion::where('temporada_id',$this->temporada->id)->where('exportadora','Greenex SpA')->get();
 
         $unique_categoriasexp = $masastotal->pluck('n_categoria')->unique()->sort();
 
