5b9848395d5373a4383b977a1b1ace50897b4b91
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2024-11-05 15:30:18 -0300

    Update Check Error

--- app/Livewire/TemporadaShow.php
+++ app/Livewire/TemporadaShow.php
@@ -373,9 +373,10 @@ class TemporadaShow extends Component
         ])
         ->get();
 
-        dd($despachosall_group);
-        
+      
+
         foreach ($despachosall_group as $item){
+            dd($item);
             Factorbalance::create([
                 'temporada_id'          => $this->temporada->id,
                 'id_empresa'            => $item->id_empresa,
