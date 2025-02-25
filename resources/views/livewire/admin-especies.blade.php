d660c488befcc0a98d142f518bfef8dfb7225997
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2025-02-25 12:41:04 -0300

    Update

--- resources/views/livewire/admin-especies.blade.php
+++ resources/views/livewire/admin-especies.blade.php
@@ -157,7 +157,7 @@
                 @foreach ($variedadsnull as $especie)
                             
                     @if ($selectedsubvariedad)
-                        @if ($selectedsubespecie->id==$especie->id)
+                        @if ($selectedsubvariedad->id==$especie->id)
                             <div class="py-3 w-full font-semibold rounded-md bg-yellow-200 transition-all duration-500 dark:bg-neutral-900 dark:hover:bg-purple-500 dark:hover:text-white ">
                                 <div class="flex items-center justify-center text-gray-600 ">
                                     {{$especie->name}}
