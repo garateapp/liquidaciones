08e3c92a2018c6ad28f2e5c6f5b59ec6603806a2
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2025-02-25 12:54:39 -0300

    Update

--- resources/views/livewire/admin-especies.blade.php
+++ resources/views/livewire/admin-especies.blade.php
@@ -146,7 +146,7 @@
                                 </div>
                             @endif
                         @else
-                            <div wire:click='updatesubespecie({{$especie->id}})' class="py-3 flex items-center justify-center w-full font-semibold rounded-md bg-orange-400 hover:bg-orange-600 text-white transition-all duration-500 dark:bg-neutral-900 dark:hover:bg-purple-500 dark:hover:text-white ">
+                            <div wire:click='updatesubespecie({{$especie->id}})' class="py-3 flex items-center justify-center w-full font-semibold rounded-md bg-green-400 hover:bg-green-600 text-white transition-all duration-500 dark:bg-neutral-900 dark:hover:bg-purple-500 dark:hover:text-white ">
                                 {{$especie->name}}
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class=" h-5 w-5 ms-3">
                                     <path fill="currentColor" d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" />
