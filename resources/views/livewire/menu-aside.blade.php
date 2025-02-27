1a513bb3e71ca101d8203fea2063bc6a246bbe8a
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2025-02-27 16:24:36 -0300

    u

--- resources/views/livewire/menu-aside.blade.php
+++ resources/views/livewire/menu-aside.blade.php
@@ -128,15 +128,15 @@
           <h1 class="mt-4">Costos 2</h1>
 
           <div class="grid grid-cols-1 gap-4 grid-cols-2 mt-2">
-              @foreach ($costomenus as $item)
+              @foreach ($costomenus as $costomenu)
                 @if ($item)
-                    <a href="{{route('costomenus.temporada',['costomenu'=>$item,'temporada'=>$temporada])}}"
+                    <a href="{{route('costomenus.temporada',['temporada'=>$temporada,'costomenu'=>$costomenu])}}"
                     wire:navigate>
                     <div class="p-2 flex flex-col items-center bg-white rounded-md justify-center shadow-xl cursor-pointer">
                       <div class="rounded-full p-2 @if($temporada->exportacions->count()>0)bg-green-200 @else bg-indigo-200 @endif  flex flex-col items-center">
                         <i class="fas fa-wallet fa-sm @if($temporada->exportacions->count()>0)text-green-600 @else text-indigo-600 @endif"></i>
                       </div>
-                      <p class="text-xs mt-1 text-center font-semibold">{{$item->name}}</p>
+                      <p class="text-xs mt-1 text-center font-semibold">{{$costomenu->name}}</p>
                     </div>
                     </a>
                 @endif
