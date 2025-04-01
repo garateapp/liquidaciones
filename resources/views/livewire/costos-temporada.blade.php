c25a2c01ccca3b738450c2899f7106cc55e2c142
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2025-04-01 02:24:42 -0300

    u

--- resources/views/livewire/costos-temporada.blade.php
+++ resources/views/livewire/costos-temporada.blade.php
@@ -20,7 +20,7 @@
         @if ($costomenus->where('name',$costomenu->name)->first()->costos->where('metodo', '!=', 'null')->count()>0)
         <div x-data="{ openTab: {{$costomenus->where('name',$costomenu->name)->first()->costos->where('metodo', '!=', 'null')->first()->id}} }" class="px-2">
             <div class="max-w-6xl mx-auto">
-                <div class="mb-4 flex space-x-4 p-2 bg-white rounded-lg border-b-2 border-gray-300">
+                <div class="mb-4 flex space-x-4 px-2 pt-2 pb-3 bg-white rounded-lg border-b-2 border-gray-300">
                 @foreach ($costomenus->where('name',$costomenu->name)->first()->costos->where('metodo', '!=', 'null') as $costo)
                     <button x-on:click="openTab = {{$costo->id}}" :class="{ 'bg-blue-600 text-white': openTab === {{$costo->id}} }" class="shadow-md flex-1 py-2 px-4 rounded-md focus:outline-none focus:shadow-outline-blue transition-all duration-300">{{$costo->name}}</button>
                 @endforeach
