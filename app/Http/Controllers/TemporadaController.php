e995166adda2855b633bad345ef52ec10f12757c
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2025-09-30 15:55:16 -0300

    u

--- app/Http/Controllers/TemporadaController.php
+++ app/Http/Controllers/TemporadaController.php
@@ -467,9 +467,11 @@ class TemporadaController extends Controller
     {   $resumes=Resumen::where('temporada_id',$temporada->id)->get();
         $CostosPackings=CostoPacking::where('temporada_id',$temporada->id)->get();
         $masitas=Balancemasa::where('temporada_id',$temporada->id)->paginate(3);
-        
+        if ($masitas->count()>0) {
             return view('temporadas.show',compact('temporada','resumes','CostosPackings'));
-       
+        } else {
+            return redirect(route('temporada.balancemasa',$temporada));
+        }
     }
 
     public function nacional(Temporada $temporada)
