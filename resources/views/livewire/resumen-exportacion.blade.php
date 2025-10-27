a512079b40a5a38fb1b2430b901aa99958007b70
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2025-10-27 17:23:52 -0300

    u

--- resources/views/livewire/resumen-exportacion.blade.php
+++ resources/views/livewire/resumen-exportacion.blade.php
@@ -121,7 +121,10 @@
               @if(!empty($detalle['regla'])) | Regla: {{ $detalle['regla'] }} @endif
             </p>
           </div>
-          <button class="text-gray-500 hover:text-gray-700" wire:click="cerrarDetalle">✕</button>
+          @if(!empty($detalle['explica']))
+               <button class="text-gray-500 hover:text-gray-700" wire:click="cerrarDetalle">✕</button>
+          @endif
+         
         </div>
 
         @if(!empty($detalle['explica']))
